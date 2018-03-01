a5d9935307c2ae7bc3e290d5b661edb50ae19a25
Safat Islam <skyscraper1709@gmail.com>
2018-03-01 20:15:47 +0600

    updates and status=can generate outputs

--- twitter/read_csv.php
+++ twitter/read_csv.php
@@ -9,15 +9,25 @@ $db = DB::getInstance('localhost', 'root', 'root', 'twitter_data');
 function getUserAverageTweetStats($tweets_array){
     $avgTweetLength = 1;
     $numberOfRTs = 0;
+    $numberOfHashtags = 0;
+    $numberOfUserMentions = 0;
     foreach ($tweets_array as $key => $tweetData) {
+        // if(strlen($tweetData['text'])>280)
+        //     var_dump(($tweetData['text']));
         $avgTweetLength += strlen($tweetData['text']);
+        $entities = json_decode($tweetData['entities_str'], true);
+        $numberOfHashtags += count($entities['hashtags']);
+        $numberOfUserMentions += count($entities['user_mentions']);
+        
         if(substr( $tweetData['text'], 0, 2 ) === "RT"){
             $numberOfRTs++;
         }
     }
     $avgTweetLength = round( $avgTweetLength / count($tweets_array) , 0);
-    $percentOfRTs = ($numberOfRTs / count($tweets_array))*100;
-    return array( 'length' => $avgTweetLength,  'rts_percent' => $percentOfRTs);
+    $percentOfRTs = round(($numberOfRTs / count($tweets_array))*100, 3);
+    $avgHashtagsPerTweet = round(($numberOfHashtags / count($tweets_array)), 3);
+    $avgUserMentionsPerTweet = round(($numberOfUserMentions / count($tweets_array)), 3);
+    return array( 'length' => $avgTweetLength,  'rts_percent' => $percentOfRTs, 'hashtagsPerTweet'=>$avgHashtagsPerTweet, 'userMentionsPerTweet' => $avgUserMentionsPerTweet);
 }
 
 function getUserTweetsinADay($tweets_array){
@@ -39,7 +49,7 @@ function getUserTweetsinADay($tweets_array){
 function createUsersData($tweets){
     //getUserList
     $userids_tweets_array = $tweets->fetchGroups('from_user');
-    echo "user_list:";
+    echo "<h1>Unique User and Data List: ".count($userids_tweets_array)."</h1>";
     lb();
     // var_dump(array_keys($userids_tweets_array));
     $u_tc = array();
@@ -54,10 +64,32 @@ function createUsersData($tweets){
         $u_tc [$user_id]['ratio'] = round( $u_tc [$user_id]['followers'] / $u_tc [$user_id]['following'], 3 );
         $u_tc [$user_id]['avg_twt_len'] = getUserAverageTweetStats($user_tweets)['length'];
         $u_tc [$user_id]['rts_percent'] = getUserAverageTweetStats($user_tweets)['rts_percent'];
+        $u_tc [$user_id]['hashtagsPerTweet'] = getUserAverageTweetStats($user_tweets)['hashtagsPerTweet'];
+        $u_tc [$user_id]['userMentionsPerTweet'] = getUserAverageTweetStats($user_tweets)['userMentionsPerTweet'];
         $u_tc [$user_id]['tweets_per_day'] = getUserTweetsinADay($user_tweets);
     }
     // arsort($u_tc);
-    var_dump($u_tc);
+    // var_dump($u_tc);
+    ?>
+    <table border="1" cellpadding="5" cellspacing="0">
+    <tr><th>@handle</th><th>totalcount</th><th>followers</th> <th>following</th> <th>ratio</th> <th>avg_twt_len</th> <th>rts_percent</th> <th>hashtagsPerTweet</th> <th>userMentionsPerTweet</th> <th>tweets_per_day</th></tr>
+    <?php
+    foreach ($u_tc as $user => $twt_stats):
+        ?>
+            <tr>
+                <td> <?php echo $user?> </td>
+            <?php
+        foreach ($twt_stats as $stat => $stat_value):
+            ?>
+            <td> <?php echo $stat_value?> </td>
+            <?php
+        endforeach;      
+        ?> </tr>  <?php
+    endforeach;
+
+    ?>
+    </table>
+    <?php
     /*
     @todo
     generate per user data in DB
@@ -68,19 +100,10 @@ function lb(){
     echo "</br>";
 }
 function getDataFromDB(){
-    // $where = array(
-    //     'page_type ='=> 'article',
-    //     'page_type NOT LIKE'  => '%öäü123',
-    //     'page_id >='=> 2,
-    // );
-
+    
     global $db;
     $tweets = $db->select('dataset');
-    // $tweets = $tweets->fetchGroups('from_user');
 
-    // echo 'There are ' . count($tweets) . ' tweets(s):' . PHP_EOL;
-
-  // var_dump($tweets);
     foreach ($tweets as $tweet) {
         // var_dump($tweet);
     }
